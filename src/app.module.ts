0cbe186588564ea21bcf8170541adf2bd22e63fc
Fabricio Alama <fabricioalama2001@outlook.es>
2026-06-03 03:14:31 -0500

    chore: estructurar modulos base del backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { PatientsModule } from './patients/patients.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { AvailabilityModule } from './availability/availability.module';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, RolesModule, PatientsModule, AppointmentsModule, AvailabilityModule, LogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
